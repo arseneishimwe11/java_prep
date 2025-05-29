f797bed5d08eb72ca2c7314ec869bbb0fe236654
arsene <arsenuga11@gmail.com>
2025-05-29 14:06:36 +0200

    Init

--- src/main/java/rw/arsene/erp/v1/dto/LoginRequest.java
+++ src/main/java/rw/arsene/erp/v1/dto/LoginRequest.java
@@ -2,21 +2,15 @@ package rw.arsene.erp.v1.dto;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 @Data
-@NoArgsConstructor
-@AllArgsConstructor
-@Builder
 public class LoginRequest {
     
-    @NotBlank(message = "Email is required")
-    @Email(message = "Please provide a valid email address")
+    @NotBlank
+    @Email
     private String email;
     
-    @NotBlank(message = "Password is required")
+    @NotBlank
     private String password;
 }
